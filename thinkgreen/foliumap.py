8c709b51b7d2675aad0c043b3b24712f0a293ef6
olilamm <olamm@vols.utk.edu>
2023-04-13 10:04:35 -0400

    create folium py

---
+++ thinkgreen/foliumap.py
@@ -0,0 +1,7 @@
+import folium 
+
+class Map(folium.Map):
+
+    def __init__(self, center=[20, 0], zoom=2, **kwargs) -> None:
+        
+        super().__init__(location=center, zoom_start=zoom, **kwargs)
