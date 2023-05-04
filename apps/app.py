fef970f1946f1826f83a8d4dbae3379ef6fd38f3
olilamm <olamm@vols.utk.edu>
2023-05-04 15:32:04 -0400

    edited site text

--- apps/app.py
+++ apps/app.py
@@ -1,114 +1,31 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
 
-st.title("Thinkgreen")
+st.set_page_config(layout="wide")
 
-st.write("This site is for my final project in Geog 510")
+# Customize the sidebar
+markdown = """
+Web App URL: <https://olilamm-thinkgreen.streamlit.app/>
+GitHub Repository: <https://github.com/olilamm/thinkgreen.git>
+"""
 
-st.text("This is some text")
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
 
-class Map(ipyleaflet.Map):
-    
-        def __init__(self, center=[20, 0], zoom=2, **kwargs) -> None:
+# Customize page title
+st.title("Streamlit for Thinkgreen")
 
-            if "scroll_wheel_zoom" not in kwargs:
-                kwargs["scroll_wheel_zoom"] = True
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project as well as a chance for me to practice building geospatial packages. You are very welcome to contribute to the [GitHub repository](https://github.com/olilamm/thinkgreen.git).
+    """
+)
 
-            super().__init__(center=center, zoom=zoom, **kwargs)
 
-            if "height" not in kwargs:
-                self.layout.height = "500px"
-            else:
-                self.layout.height = kwargs["height"]
+st.markdown(markdown)
 
-            if "fullscreen_control" not in kwargs:
-                kwargs["fullscreen_control"] = True
-            if kwargs["fullscreen_control"]:
-                self.add_fullscreen_control()
-            
-            if "layers_control" not in kwargs:
-                kwargs["layers_control"] = False
-            if kwargs["layers_control"]:
-                self.add_layers_control()
-
-        def add_search_control(self, position="topleft", **kwargs):
-            """Adds a search control to the map.
-            Args:
-                kwargs: Keyword arguments to pass to the search control.
-            
-            Returns:
-                ipyleaflet.SearchControl: The search control.
-            """
-            if "url" not in kwargs:
-                kwargs["url"] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
-        
-
-            search_control = ipyleaflet.SearchControl(position=position, **kwargs)
-            self.add_control(search_control)
-
-        def add_draw_control(self, **kwargs):
-            """Adds a draw control to the map.
-            Args:
-                kwargs: Keyword arguments to pass to the draw control.
-            
-            Returns:
-                ipyleaflet.DrawControl: Draw control.
-            """
-            draw_control = ipyleaflet.DrawControl(**kwargs)
-
-            draw_control.polyline =  {
-                "shapeOptions": {
-                    "color": "#6bc2e5",
-                    "weight": 8,
-                    "opacity": 1.0
-                }
-            }
-            draw_control.polygon = {
-                "shapeOptions": {
-                    "fillColor": "#6be5c3",
-                    "color": "#6be5c3",
-                    "fillOpacity": 1.0
-                },
-                "drawError": {
-                    "color": "#dd253b",
-                    "message": "Oups!"
-                },
-                "allowIntersection": False
-            }
-            draw_control.circle = {
-                "shapeOptions": {
-                    "fillColor": "#efed69",
-                    "color": "#efed69",
-                    "fillOpacity": 1.0
-                }
-            }
-            draw_control.rectangle = {
-                "shapeOptions": {
-                    "fillColor": "#fca45d",
-                    "color": "#fca45d",
-                    "fillOpacity": 1.0
-                }
-            }
-
-            self.add_control(draw_control)
-
-        def add_layers_control(self, position="topright"):
-            """Adds a layers control to the map.
-            Args:
-                kwargs: Keyword arguments to pass to the layers control.
-            
-            Returns: 
-                ipyleaflet.LayersControl: The layers control.
-            """
-            layers_control = ipyleaflet.LayersControl(position=position)
-            self.add_control(layers_control)
-
-        def add_fullscreen_control(self, position="topleft"):
-            """Adds a fullscreen control to the map.
-            Args:
-                kwargs: Keyword arguments to pass to the fullscreen control.
-
-            Returns:
-                ipyleaflet.FullscreenControl: Allows control of screensize.
-            """
-            fullscreen_control = ipyleaflet.FullScreenControl(position=position)
-            self.add_control(fullscreen_control)
+m = leafmap.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
+m.to_streamlit(height=500)
